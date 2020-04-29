71930c352da45db31277c954e26435e7e7f14fdc
hansa-lallu <hansalallu@gmail.com>
2020-04-29 13:17:36 +0100

    fixed rubocop violations

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,8 +1,8 @@
-class LeapYear 
+class LeapYear
   def leap_year?(year)
     return true if year % 400 == 0
-    return false if year % 100 === 0 && year % 400 != 0
-    return true if year % 4 == 0 && year % 100 !=0
+    return false if year % 100 == 0 && year % 400 != 0
+    return true if year % 4 == 0 && year % 100 != 0
     return false if year % 4 != 0
   end
-end 
+end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -1,6 +1,6 @@
 require 'leap_year'
 
-describe LeapYear do 
+describe LeapYear do
   describe '#leap_year?' do
     it 'returns true for all years that are divisable by 400' do
       year = 2000
@@ -12,21 +12,19 @@ describe LeapYear do
       expect(subject.leap_year?(year)).to eq(false)
     end
 
-    it 'returns false for all years divisable by 100 but not 400' do 
+    it 'returns false for all years divisable by 100 but not 400' do
       year = 1700
       expect(subject.leap_year?(year)).to eq(false)
-    end 
+    end
 
-    it 'returns true for all years divisable by 4 and not by 100' do 
-      year = 2004 
+    it 'returns true for all years divisable by 4 and not by 100' do
+      year = 2004
       expect(subject.leap_year?(year)).to eq(true)
-    end 
+    end
 
     it 'returns false for all years that are not divisable by 4' do
       year = 2009
       expect(subject.leap_year?(year)).to eq(false)
     end
-  end 
-end 
-
-
+  end
+end
